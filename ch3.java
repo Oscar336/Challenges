c48f1e97b98744ece6564bf2bb47bd005bd5753e
Oscar336 <33257633+Oscar336@users.noreply.github.com>
2017-10-31 14:22:02 +0000

    Create ch3.java

---
+++ ch3.java
@@ -0,0 +1,20 @@
+package challenge3.pkg8;
+
+import java.util.Scanner;
+
+public class Challenge38 {
+
+    public static void main(String[] args) {
+       Scanner trevor = new Scanner(System.in); 
+        System.out.println("Enter your number");
+        int age = trevor.nextInt();
+        
+        if (age >100) {
+            System.out.println("larger than 100");  
+        }else{
+            System.out.println("Less than 100");  
+        
+    }
+    
+}
+}
