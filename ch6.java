cde338ffeba216fcc3f42e93503c76b90ba438cb
Oscar336 <33257633+Oscar336@users.noreply.github.com>
2017-10-31 14:28:15 +0000

    Create ch6.java

---
+++ ch6.java
@@ -0,0 +1,17 @@
+package challenge6.pkg11;
+
+import java.util.Scanner;
+
+public class Challenge611 {
+
+    
+    public static void main(String[] args) {
+    Scanner trevor = new Scanner(System.in);
+      System.out.println("Insert 1st numbers");
+      int number1 = trevor.nextInt();
+      System.out.println("Insert 2nd numbers");
+      int number2 = trevor.nextInt();
+      System.out.println(number1*number2);
+    }
+    
+}
