b34c8e07bd60a33f77b516ebc0b0d79e05adb5b5
Oscar336 <33257633+Oscar336@users.noreply.github.com>
2017-10-31 14:15:47 +0000

    Create challenge1.java

---
+++ challenge1.java
@@ -0,0 +1,19 @@
+package challenges6;
+
+
+public class Challenges1 {
+
+    
+    public static void main(String[] args) {
+        
+    
+
+        System.out.println("France");
+       
+        
+        int population = 66900000;
+        
+        System.out.println("The population of France is: " + population);
+    }
+}
+
