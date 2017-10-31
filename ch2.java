829d5e9159bd3c0657d5b72968cd67a540e7ea6b
Oscar336 <33257633+Oscar336@users.noreply.github.com>
2017-10-31 14:19:00 +0000

    Create ch2.java

---
+++ ch2.java
@@ -0,0 +1,20 @@
+package challenge.pkg2.pkg7;
+
+import  java.util.Scanner;
+
+public class Challenge27 {
+
+    
+    public static void main(String[] args) {
+        
+      Scanner trevor = new Scanner(System.in);
+      System.out.println("Insert 1st numbers");
+      int number1 = trevor.nextInt();
+      System.out.println("Insert 2nd numbers");
+      int number2 = trevor.nextInt();
+      
+        System.out.println(number1 + "multipled by " + "" + number1*number2);
+      
+      
+    }
+}
